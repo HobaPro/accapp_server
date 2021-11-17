854f65681b84bbd5228fd805a621aa360181e668
Mohab <79037221+HobaProo@users.noreply.github.com>
2021-11-17 18:31:11 +0200

    Edit2

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.use(
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    //res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Content-Type,application');
+    next();
+})
+
 const authRouter = require('./routes/auth.route')
 
 app.use(authRouter);

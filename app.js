caeb64ef2fa488c9740b4f5358ae2c2f0576f49c
Mohab <79037221+HobaProo@users.noreply.github.com>
2021-11-18 05:25:53 +0200

    accapp

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
 
-app.use(
+/*app.use(
     cors({
         origin: "*",
     })
-)
+)*/
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    //res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Content-Type,application');
-    next();
-})
 
 app.get('/', (req, res) => {
     res.send("Hellooooooooo");
